e45d3b4921609abefa221539c184fb6bf2d7c529
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2020-05-13 06:52:02 -0500

    Only run the loss printing code when requested (testing)

--- test.py
+++ test.py
@@ -63,11 +63,12 @@ if __name__ == '__main__':
         img_path = model.get_image_paths()     # get image paths
         if i % 5 == 0:  # save images to an HTML file
             print('processing (%04d)-th image... %s' % (i, img_path))
-        losses = model.get_current_losses()
-        for k, v in losses.items(): # we know there only exist G_L1
-            print('%s: %.4f' % (k, v), end = "\t") # print current loss
-            total_loss[k].append(v)
-        print("\n", end = "")
+        if self.test_loss == 1:
+            losses = model.get_current_losses()
+            for k, v in losses.items(): # we know there only exist G_L1
+                print('%s: %.4f' % (k, v), end = "\t") # print current loss
+                total_loss[k].append(v)
+            print("\n", end = "")
         save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
     webpage.save()  # save the HTML
 
